a5049a3e6182dd9e2160868acefda975a91f25c8
santiagoMicMkt <santiago.martinez@millicom.com>
2023-10-04 16:51:54 -0500

    se agregaron las propiedades de valor, numero, correo, category, action y label a los scripts de formulario enviado y formulario exitoso

--- formulario_enviado.js
+++ formulario_enviado.js
@@ -9,6 +9,7 @@ function handleFormSubmit() {
       label: 'HN',
       bu: 'POSTPAGO',
       oferta: 'PLAN MÓVIL 10 GB',
+      valor: 9.99,
       numero: '5c91117b2ba54a20e53f49a2098f03c89a5f78d4a9ff9fb3bedd8fe4f826a41a',
       correo: '8095ac28eb371aa20fff24e7fca1dea95fac0fa282e399fe1e0ad72ae304135e',
  });
